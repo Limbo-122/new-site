f645bf73783e4c5e8232426736b78347b34b4c5d
Limbo-122 <lukicevroman88@gmail.com>
2024-10-27 13:11:42 +0300

    Create main.py
    uhh so uhh no

---
+++ main.py
@@ -0,0 +1,34 @@
+#this is index.html
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <title>Its something</title>
+          <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+          <h1>Gamers problem</h1>
+          <p>Always in pc cant even go get water lol...</p>
+          <h2>If you do the same then</h2>
+                   <ul>
+                         <li>Go get a life </li>
+                         <li>Get some friends (not in vidio game)</li>
+                   </ul>
+           <img src="https://t3.ftcdn.net/jpg/02/85/90/44/360_F_285904422_IdADB85m2sNiBDvpEwIdRnOkbSNki9Kq.jpg">
+      </body>
+      </html>
+#this is style.css
+      body {
+          font-family: Arial,  sans-serif; /* Семейство шрифтов */
+          font-size: 11px; /* Размер основного шрифта в <p></p>  */
+          background-color: #f0f0f0; /* Цвет фона веб-страницы */
+          color: #333333; /* Цвет основного текста */ 
+      }
+      h1 {
+          color: #00a800; /* Цвет заголовка */
+          font-size: 24px; /* Размер шрифта в пунктах */
+          font-family: Georgia, serif; /* Семейство шрифтов */
+      }
+      img{
+          width: 100%;
+          height: 100%;
+      }
